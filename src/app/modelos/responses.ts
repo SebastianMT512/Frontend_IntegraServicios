8c8bb6fc533d8767d7c52ae095d54a511a7b84d5
SebastianMT512 <sebastianmt512@gmail.com>
2025-02-12 09:56:58 -0500

    Update responses.ts

--- src/app/modelos/responses.ts
+++ src/app/modelos/responses.ts
@@ -1,27 +1,27 @@
-export interface estampaResponse{
-    data:Array<any>
+export interface estampaResponse {
+   data: Array<any>;
 }
 
-export interface LogResponse{
-    message:string
-    codigo:number
+export interface LogResponse {
+   message: string;
+   codigo: number;
 }
 
-export interface categoriaSeleccionadaResponse{
-    data: Array<any>
-    recursos_diponibles : Array<any>
+export interface categoriaSeleccionadaResponse {
+   data: Array<any>;
+   recursos_disponibles: Array<any>;
 }
 
-export interface reservaActivaResponse{
-    data:Array<any>
+export interface reservaActivaResponse {
+   data: Array<any>;
 }
 
-export interface historialReservaResponse{
-    data:Array<any>
+export interface historialReservaResponse {
+   data: Array<any>;
 }
 
-export interface reservaSeleccionadaResponse{
-    data:Array<any>
+export interface reservaSeleccionadaResponse {
+   data: Array<any>;
 }
 
 export interface ApiResponseV2 {
